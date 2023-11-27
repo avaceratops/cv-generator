e4888cb1e596de62cea344dfafa65145b58cde9b
ava <148492613+avaceratops@users.noreply.github.com>
2023-11-27 18:15:13 +0000

    Add remaining actions to dataReducer
    These implement functionality for handling changes to
    Education/Experience sections and their respective forms.
    
    Actions added:
    - add-section
    - change-section
    - delete-section
    - toggle-section

--- src/dataReducer.js
+++ src/dataReducer.js
@@ -1,7 +1,47 @@
+let nextId = 0;
+
+function createNewSection(category) {
+  const education = { institute: '', qualification: '' };
+  const experience = { employer: '', position: '' };
+
+  const base = {
+    id: nextId++,
+    isOpen: true,
+    location: '',
+    yearStart: '',
+    yearEnd: '',
+    desc: '',
+  };
+  return Object.assign({}, base, category === 'education' ? education : experience);
+}
+
+function findSectionById(array, id) {
+  return array.find((s) => s.id == id);
+}
+
 export default function dataReducer(draft, action) {
   switch (action.type) {
     case 'change-personal': {
       draft.personal[action.key] = action.value;
+      break;
+    }
+    case 'add-section': {
+      const newSection = createNewSection(action.category);
+      draft[action.category].push(newSection);
+      break;
+    }
+    case 'change-section': {
+      const section = findSectionById(draft[action.category], action.id);
+      section[action.key] = action.value;
+      break;
+    }
+    case 'delete-section': {
+      draft[action.category] = draft[action.category].filter((s) => s.id != action.id);
+      break;
+    }
+    case 'toggle-section': {
+      const section = findSectionById(draft[action.category], action.id);
+      section.isOpen = !section.isOpen;
       break;
     }
     default: {
