3c167e58cd2eb314965f66fa0075e654136d4123
ava <148492613+avaceratops@users.noreply.github.com>
2023-11-27 17:48:12 +0000

    Add EducationForm component
    Accepts an array of sections and conditionally renders a Sections
    component. When a section is added/edited via the corresponding
    buttons, the component switches to rendering a form instead.
    
    The form provides inputs for the user to enter details for the
    individual section, such as institute, qualification, and year.
    Buttons are included for canceling/saving any changes.

---
+++ src/components/EducationForm.jsx
@@ -0,0 +1,79 @@
+import Form from './Form';
+import FormField from './FormField';
+import Sections from './Sections';
+
+export default function EducationForm({
+  education,
+  onAddSection,
+  onChangeSection,
+  onDeleteSection,
+  onToggleSection,
+}) {
+  const openSection = education.find((section) => section.isOpen);
+
+  return (
+    <section className="section-container">
+      {openSection ? (
+        <section data-category="education" data-id={openSection.id}>
+          <Form onToggleSection={onToggleSection}>
+            <FormField
+              id="institute"
+              label="Institute"
+              value={openSection.institute}
+              onChange={onChangeSection}
+              required={true}
+            />
+            <FormField
+              id="location"
+              label="Location"
+              value={openSection.location}
+              onChange={onChangeSection}
+            />
+            <FormField
+              id="qualification"
+              label="Qualification"
+              value={openSection.qualification}
+              onChange={onChangeSection}
+            />
+            <FormField
+              id="yearStart"
+              label="Year started"
+              value={openSection.yearStart}
+              onChange={onChangeSection}
+            />
+            <FormField
+              id="yearEnd"
+              label="Year ended"
+              value={openSection.yearEnd}
+              onChange={onChangeSection}
+            />
+            <FormField
+              id="desc"
+              label="Description"
+              type="textarea"
+              value={openSection.desc}
+              onChange={onChangeSection}
+            />
+            <section className="button-container button-container--editing">
+              <button className="button button--cancel" type="button" onClick={onToggleSection}>
+                Cancel
+              </button>
+              <button className="button button--save" type="submit">
+                Save
+              </button>
+            </section>
+          </Form>
+        </section>
+      ) : (
+        <Sections
+          array={education}
+          category="education"
+          headingKey="institute"
+          onAddSection={onAddSection}
+          onDeleteSection={onDeleteSection}
+          onToggleSection={onToggleSection}
+        />
+      )}
+    </section>
+  );
+}
