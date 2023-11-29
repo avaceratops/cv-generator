58927f40e62464106b0a0fd9ebb492e1cdb409f5
ava <148492613+avaceratops@users.noreply.github.com>
2023-11-29 10:43:04 +0000

    Add icon support to ConfirmDialog
    Added new prop triggerIcon. This will conditionally render an icon for
    the trigger button, using the specified Font Awesome icon name.
    
    A hidden span is included to ensure accessibility for screen readers.

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -2,16 +2,25 @@ import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import '../styles/ConfirmDialog.scss';
 
 export default function ConfirmDialog({
+  triggerIcon,
   triggerClass,
   triggerText,
   title = 'Are you sure?',
   desc = 'This action cannot be undone.',
   actionOnClick,
 }) {
+  // accept both 'fa-icon-name' and 'icon-name'
+  if (triggerIcon && !triggerIcon.startsWith('fa-')) {
+    triggerIcon = `fa-${triggerIcon}`;
+  }
+
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger asChild>
-        <button className={triggerClass}>{triggerText}</button>
+        <button className={triggerClass}>
+          {triggerIcon && <i className={`fa-solid ${triggerIcon}`} aria-hidden="true"></i>}
+          <span className={triggerIcon ? 'fa-sr-only' : ''}>{triggerText}</span>
+        </button>
       </AlertDialog.Trigger>
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="dialog-overlay" />
